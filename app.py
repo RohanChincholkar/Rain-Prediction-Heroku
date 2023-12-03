a873f29817afc10eea4813175ae3ae38b24d9269
Rohan Chincholkar <rohanchincholkar321@gmail.com>
2023-12-03 20:37:19 +0530

    rendering plus form conversion

--- app.py
+++ app.py
@@ -13,7 +13,13 @@ def home():
 def index():
     if request.method == 'POST':
         # Process the form data here
-        input_data = request.form.getlist()
+        input_data = request.form.getlist('Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine',
+       'WindGustDir', 'WindGustSpeed', 'WindDir9am', 'WindDir3pm',
+       'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 'Humidity3pm',
+       'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am',
+       'Temp3pm', 'RainToday', 'year', 'month_sin', 'month_cos', 'day_sin',
+       'day_cos')
+        input_data= list(map(float, input_data))
         # Do something with the input_data list
         with open('model.pkl', 'rb') as f:
             model = pickle.load(f)
@@ -21,7 +27,7 @@ def index():
     
 
 
-    return render_template('index.html')
+
     
     if __name__ == "__main__":
     app.run()
