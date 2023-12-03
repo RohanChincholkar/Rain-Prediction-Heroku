954357e624a22466b70c388391a9500fc5a4bd14
Rohan Chincholkar <rohanchincholkar321@gmail.com>
2023-12-03 20:20:36 +0530

    prediction rendering

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,render_template_string, request
+import pickle
 
 
 app = Flask(__name__)
@@ -7,5 +8,20 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == "__main__":
+
+@app.route('/predict', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Process the form data here
+        input_data = request.form.getlist()
+        # Do something with the input_data list
+        with open('model.pkl', 'rb') as f:
+            model = pickle.load(f)
+        return render_template_string( "Prediction of rain is : " + str(model.predict(input_data)[0]))
+    
+
+
+    return render_template('index.html')
+    
+    if __name__ == "__main__":
     app.run()
