d918caaf1e2f6806f871ed9efa67598960afdc05
Rohan Chincholkar <rohanchincholkar321@gmail.com>
2023-12-03 15:00:33 +0530

    2nd commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask heroku app."
+    return render_template('./templates/index.html')
 
 if __name__ --"__main__":
     app.run()
