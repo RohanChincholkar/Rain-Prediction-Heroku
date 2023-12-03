a08873adfe1135efede1e9e184ac92b8c192043a
Rohan Chincholkar <rohanchincholkar321@gmail.com>
2023-12-03 20:25:03 +0530

    xknweclwe

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def index():
         # Do something with the input_data list
         with open('model.pkl', 'rb') as f:
             model = pickle.load(f)
-        return render_template_string( "Prediction of rain is : " + str(model.predict(input_data)[0]))
+        return render_template_string( "Prediction of rain is : " + str(model.predict(input_data)))
     
 
 
