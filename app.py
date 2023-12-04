d868b90f7e5e60d109d05db2eb6f5bb35bc1cc94
Rohan Chincholkar <rohanchincholkar321@gmail.com>
2023-12-04 11:39:33 +0530

    dict to values

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,render_template_string, request, redirect
 import pickle
+import json
 
 
 app = Flask(__name__)
@@ -13,12 +14,17 @@ def home():
 def index():
     if request.method == 'POST':
         # Process the form data here
-        input_data = request.form['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine',
-       'WindGustDir', 'WindGustSpeed', 'WindDir9am', 'WindDir3pm',
-       'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 'Humidity3pm',
-       'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am',
-       'Temp3pm', 'RainToday', 'year', 'month_sin', 'month_cos', 'day_sin',
-       'day_cos']
+    #     input_data = request.form['Location', 'MinTemp', 'MaxTemp', 'Rainfall', 'Evaporation', 'Sunshine',
+    #    'WindGustDir', 'WindGustSpeed', 'WindDir9am', 'WindDir3pm',
+    #    'WindSpeed9am', 'WindSpeed3pm', 'Humidity9am', 'Humidity3pm',
+    #    'Pressure9am', 'Pressure3pm', 'Cloud9am', 'Cloud3pm', 'Temp9am',
+    #    'Temp3pm', 'RainToday', 'year', 'month_sin', 'month_cos', 'day_sin',
+    #    'day_cos']
+        input_data = request.get_json()
+        python_dict = json.loads(input_data)
+        input_data = [value for key, value in python_dict.items()]
+
+
         input_data= list(map(float, input_data))
         with open('model.pkl', 'rb') as f:
             model = pickle.load(f)
